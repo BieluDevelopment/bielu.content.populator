f0a268c271691f14da7d3f0b5a297aec7f3cae2c
Arkadiusz biel <arkadiusz.biel@etchuk.com>
2023-02-12 15:22:00 +0000

    refactor: unified methoed to handle to report and state for all types

---
+++ src/Bielu.Content.Populator.Models/Report/ContentPopulatorImportContext.cs
@@ -0,0 +1,8 @@
+﻿namespace Bielu.Content.Populator.Models.Report;
+
+public class ContentPopulatorImportContext
+{
+    public StateReport State { get; set; }
+    public string HistoryPath { get; set; }
+    public string DataDirectory { get; set; }
+}
--- src/Bielu.Content.Populator.Umbraco.Core/Services/ImportService.cs
+++ src/Bielu.Content.Populator.Umbraco.Core/Services/ImportService.cs
@@ -1,5 +1,7 @@
 ﻿using System.Text;
 using Bielu.Content.Populator.Models;
+using Bielu.Content.Populator.Models.Report;
+
 using Bielu.Content.Populator.Services;
 using Microsoft.AspNetCore.Hosting;
 using Newtonsoft.Json;
@@ -32,41 +34,40 @@ public class ImportService : IImportService
 
     public bool Import()
     {
-        var dataDir = Path.Combine(_env.ContentRootPath, $"ContentPopulatordata");
-        var dir = Path.Combine(dataDir, "HistoryLogs");
-        EnsureBaseDirectories(dataDir, dir);
-        var reportFilePath = Path.Combine(dataDir, "state.json");
-        StateReport state = File.Exists(reportFilePath) ? GetFile(reportFilePath) : new StateReport();
+        var context = PrepareContext();
         ChangesReport changeLog = new ChangesReport();
-        IList<ContentDefitinion> configurations = _configuration.GetAllConfigurations(state);
+        IList<ContentDefitinion> configurations = _configuration.GetAllConfigurations(context.State);
         changeLog.DataTypes = _dataTypeImportService.Import(configurations);
         changeLog.ContentTypes = _contentTypeImportService.Import(configurations);
         changeLog.Content = _contentImportService.Import(configurations);
         if (configurations.Any())
         {
-          
-
-            dir = Path.Combine(dir, $"history-{DateTime.UtcNow.ToString("yyyy-MM-dd-hh-mm")}.json");
-
-            using (FileStream fs = System.IO.File.Create(dir))
-            {
-                byte[] content =
-                    new UTF8Encoding(true).GetBytes(JsonConvert.SerializeObject(changeLog, _jsonSerializerSettings));
-
-                fs.Write(content, 0, content.Length);
-            }
-            dir = Path.Combine(dataDir, $"state.json");
-
-            using (FileStream fs = System.IO.File.Create(dir))
-            {
-                byte[] content =
-                    new UTF8Encoding(true).GetBytes(JsonConvert.SerializeObject(state, _jsonSerializerSettings));
-
-                fs.Write(content, 0, content.Length);
-            }
+            ReportChanges(configurations, context, changeLog);
         }
 
         return true;
+    }
+
+    private void ReportChanges(IList<ContentDefitinion> configurations, ContentPopulatorImportContext context, ChangesReport changeLog)
+    {
+        var  dir = Path.Combine(context.HistoryPath, $"history-{DateTime.UtcNow.ToString("yyyy-MM-dd-hh-mm")}.json");
+
+        using (FileStream fs = System.IO.File.Create(dir))
+        {
+            byte[] content =
+                new UTF8Encoding(true).GetBytes(JsonConvert.SerializeObject(changeLog, _jsonSerializerSettings));
+
+            fs.Write(content, 0, content.Length);
+        }
+        dir = Path.Combine(context.DataDirectory, $"state.json");
+
+        using (FileStream fs = System.IO.File.Create(dir))
+        {
+            byte[] content =
+                new UTF8Encoding(true).GetBytes(JsonConvert.SerializeObject(context.State, _jsonSerializerSettings));
+
+            fs.Write(content, 0, content.Length);
+        }
     }
 
     private void EnsureBaseDirectories(string dataDir, string dir)
@@ -82,47 +83,64 @@ public class ImportService : IImportService
         }
     }
 
-    private StateReport GetFile(string reportFilePath)
+    private StateReport GetState(string reportFilePath)
     {
         return JsonConvert.DeserializeObject<StateReport>(File.ReadAllText(reportFilePath));
     }
 
-    public bool ImportDataTypes()
+    private ContentPopulatorImportContext PrepareContext()
     {
         var dataDir = Path.Combine(_env.ContentRootPath, $"ContentPopulatordata");
         var dir = Path.Combine(dataDir, "HistoryLogs");
         EnsureBaseDirectories(dataDir, dir);
         var reportFilePath = Path.Combine(dataDir, "state.json");
-        StateReport state = File.Exists(reportFilePath) ? GetFile(reportFilePath) : new StateReport();
+        StateReport state = File.Exists(reportFilePath) ? GetState(reportFilePath) : new StateReport();
+        return new ContentPopulatorImportContext()
+        {
+            State = state,
+            HistoryPath = dir,
+            DataDirectory = dataDir
+        };
+    }
+
+    public bool ImportDataTypes()
+    {
+       
+        var context = PrepareContext();
         ChangesReport changeLog = new ChangesReport();
-        IList<ContentDefitinion> configurations = _configuration.GetAllConfigurations(state);
+        IList<ContentDefitinion> configurations = _configuration.GetAllConfigurations(context.State).Where(x=>x.DataTypes.Any()).ToList();
         changeLog.DataTypes = _dataTypeImportService.Import(configurations);
+        if (configurations.Any())
+        {
+            ReportChanges(configurations, context, changeLog);
+        }
+       
         return true;
     }
 
     public bool ImportContentTypes()
     {
-        var dataDir = Path.Combine(_env.ContentRootPath, $"ContentPopulatordata");
-        var dir = Path.Combine(dataDir, "HistoryLogs");
-        EnsureBaseDirectories(dataDir, dir);
-        var reportFilePath = Path.Combine(dataDir, "state.json");
-        StateReport state = File.Exists(reportFilePath) ? GetFile(reportFilePath) : new StateReport();
+           
+        var context = PrepareContext();
         ChangesReport changeLog = new ChangesReport();
-        IList<ContentDefitinion> configurations = _configuration.GetAllConfigurations(state);
-        changeLog.ContentTypes = _contentTypeImportService.Import(configurations);
+        IList<ContentDefitinion> configurations = _configuration.GetAllConfigurations(context.State).Where(x=>x.ContentTypes.Any()).ToList();
+        changeLog.DataTypes = _contentTypeImportService.Import(configurations);
+        if (configurations.Any())
+        {
+            ReportChanges(configurations, context, changeLog);
+        }
         return true;
     }
 
     public bool ImportContent()
-    {
-        var dataDir = Path.Combine(_env.ContentRootPath, $"ContentPopulatordata");
-        var dir = Path.Combine(dataDir, "HistoryLogs");
-        EnsureBaseDirectories(dataDir, dir);
-        var reportFilePath = Path.Combine(dataDir, "state.json");
-        StateReport state = File.Exists(reportFilePath) ? GetFile(reportFilePath) : new StateReport();
+    {var context = PrepareContext();
         ChangesReport changeLog = new ChangesReport();
-        IList<ContentDefitinion> configurations = _configuration.GetAllConfigurations(state);
-        changeLog.Content = _contentImportService.Import(configurations);
+        IList<ContentDefitinion> configurations = _configuration.GetAllConfigurations(context.State).Where(x=>x.Content.Any()).ToList();
+        changeLog.DataTypes = _contentImportService.Import(configurations);
+        if (configurations.Any())
+        {
+            ReportChanges(configurations, context, changeLog);
+        }
         return true;
     }
 }
